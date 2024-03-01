3e9eb7ea0737de3c068b8c056d4d0d2665215fe1
akmaljon <akmaljon@gmail.com>
2024-03-01 19:03:37 +0500

    ergvtrhb

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import NavBar from "./components/NavBar";
-import Home from "./pages/Home";
-import Video from "./pages/Video";
-import Tv from "./pages/Tv";
-import BookMark from "./pages/BookMark";
-import ErrorPage from "./pages/ErrorPage";
+import Page from "./components/Page";
+import { GlobalStyles } from "./styles/Global.styled";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./styles/Theme";
 
 function App() {
-
   return (
-    <>
-      <BrowserRouter>
-        <NavBar />
-        <Routes>
-          <Route path="/reactproject5/" element={<Home />} />
-          <Route path="/reactproject5/video" element={<Video />} />
-          <Route path="/reactproject5/tv" element={<Tv />} />
-          <Route path="/reactproject5/bookmark" element={<BookMark />} />
-          <Route path="/*" element={<ErrorPage />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Page />
+    </ThemeProvider>
   );
 }
 
